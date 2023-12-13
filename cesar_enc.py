e866a9a71fd080ec2521d923bf70f7494f66d7f3
Faris Ahmed Yahya <fariszking@gmail.com>
2023-12-13 16:37:23 +0100

    Add files via upload

---
+++ cesar_enc.py
@@ -0,0 +1,32 @@
+import string
+
+class Caesar:
+    def __init__(self, message: str, key: int) -> None:
+        self.message = message.lower()  # Corrected: Added parentheses to call lower()
+        self.key = key
+        self.alpha = string.ascii_lowercase
+
+    def encryption(self) -> str:
+        encrypted_message = ''
+        for char in self.message:
+            if char in self.alpha:  # Checking if the character is in the alphabet
+                char_index = (self.alpha.index(char) + self.key) % 26
+                encrypted_message += self.alpha[char_index]
+            else:
+                encrypted_message += char  # Keep non-alphabetic characters unchanged
+        return encrypted_message    
+    def decryption(self) -> str:
+        decypted_message = ''
+        enc_message = self.encryption()
+        for char in enc_message:
+            if char in self.alpha:
+                char_index = (self.alpha.index(char) - self.key) % 26
+                decypted_message += self.alpha[char_index]
+            else:
+                decypted_message += char
+        return decypted_message
+
+
+cesar = Caesar('nesoacademy',3)
+print(cesar.encryption())
+print(cesar.decryption())
